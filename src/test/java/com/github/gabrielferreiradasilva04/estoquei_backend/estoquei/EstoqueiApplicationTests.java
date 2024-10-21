aa9c4eb7ffcbe38c5cbba71da712607dbefdba6a
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2024-10-20 21:31:21 -0300

    primeiro-commit

---
+++ src/test/java/com/github/gabrielferreiradasilva04/estoquei_backend/estoquei/EstoqueiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielferreiradasilva04.estoquei_backend.estoquei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
