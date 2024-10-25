c2b56c61ea63d6300554e72d383ea631595e9557
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2024-10-25 12:28:09 -0300

    criada a classe duplicateException

---
+++ src/main/java/com/github/gabrielferreiradasilva04/estoquei_backend/estoquei/exceptions/DuplicateException.java
@@ -0,0 +1,9 @@
+package com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.exceptions;
+
+public class DuplicateException extends RuntimeException{
+	
+	public DuplicateException(String message) {
+		super(message);
+	}
+
+}
