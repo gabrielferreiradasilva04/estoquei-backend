4942aeff2ec92341182055c4d51b5d0e7ecfd97a
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2024-10-28 00:20:21 -0300

    feita a configuração do cors

--- src/main/java/com/github/gabrielferreiradasilva04/estoquei_backend/estoquei/controller/AuthenticationController.java
+++ src/main/java/com/github/gabrielferreiradasilva04/estoquei_backend/estoquei/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +14,17 @@ import com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.config.Secu
 import com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.entity.UserEntity;
 import com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.entity.dtos.AuthenticationDto;
 import com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.entity.dtos.LoginResponseDto;
+import com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.entity.dtos.UserEntityDto;
 import com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.repository.UserRepository;
 import com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.service.TokenService;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthenticationController {
 	
 	private AuthenticationManager authenticationManager;
@@ -34,7 +38,7 @@ public class AuthenticationController {
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody AuthenticationDto auth, HttpServletResponse response){
+	public ResponseEntity<?> login(@RequestBody @Valid AuthenticationDto auth, HttpServletResponse response){
 		//processo de autenticação
 		var userNamePassword = new UsernamePasswordAuthenticationToken(auth.email(), auth.password());
 		var authenticate = this.authenticationManager.authenticate(userNamePassword);
@@ -44,14 +48,20 @@ public class AuthenticationController {
 		Cookie jwtCookie = new Cookie(SecurityFilterConfiguration.TOKEN_JWT, token);
 		jwtCookie.setHttpOnly(true); //estou protegendo o meu token contra scripts javascript
 		jwtCookie.setPath("/"); //especificando para quais endpoints o token vai ser mandado
-		jwtCookie.setMaxAge(24*60*60);//define a expiração do token para 1 dia inteiro
+		jwtCookie.setMaxAge(60*60);//define a expiração do token para 1 dia inteiro
+		jwtCookie.setSecure(false);
+		 //adicionando o atributo sameSite no header de retorno do cookie para que ele seja salvo em qualquer requisição
+		String sameSite = "None";
+		response.addHeader("Set-Cookie", String.format("%s=%s; HttpOnly; Path=%s; Max-Age=%d; SameSite=%s; Secure", 
+				jwtCookie.getName(), jwtCookie.getValue(), jwtCookie.getPath(), jwtCookie.getMaxAge(), sameSite));
 		response.addCookie(jwtCookie);
 		//retornar resposta ao cliente.
 		return ResponseEntity.ok(new LoginResponseDto(token));
 	}
 	
 	@PostMapping("/register")
-	public ResponseEntity<?> register(@RequestBody UserEntity user){
+	public ResponseEntity<?> register(@RequestBody @Valid UserEntityDto userDto){
+		UserEntity user = userDto.toUserEntity(); 
 		if(this.userRepository.findByEmail(user.getEmail()) != null) {
 			return ResponseEntity.badRequest().build();
 		}else {
--- src/main/java/com/github/gabrielferreiradasilva04/estoquei_backend/estoquei/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/github/gabrielferreiradasilva04/estoquei_backend/estoquei/controller/common/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.github.gabrielferreiradasilva04.estoquei_backend.estoquei.controller.common;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,4 +41,14 @@ public class GlobalExceptionHandler {
 				fieldErrorsDTO
 				);
 	}
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	@ResponseStatus(HttpStatus.FORBIDDEN)
+	public ResponseErrorDto handleAccessDeniedException(AccessDeniedException e) {
+		return new ResponseErrorDto(
+				HttpStatus.FORBIDDEN.value(),
+				"Acesso negado", null
+				);
+	}
+
 }
